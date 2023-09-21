814c1394b9c649c85061ee48d13a13afc30b8b69
femilana <phemeelana@gmail.com>
2023-09-21 07:05:52 -0700

    main.h

--- main.h
+++ main.h
@@ -1,7 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
-#include <stdarg.h> //to use variable argument list
-#include <string.h> //to use strlen
+#include <stdarg.h>
+#include <string.h> 
 
 int _printf(const char *format, ...);
 
